3ada285cb2e6d7f94064fecd53e8ecabd33d5b79
Fábio <fabioovila@gmail.com>
2025-12-05 17:12:11 +0000

    abrasileirei termos da interface crud

--- src/CrudInterface.h
+++ src/CrudInterface.h
@@ -6,8 +6,8 @@ class CrudInterface {
 public:
     virtual ~CrudInterface() {}
 
-    virtual bool create(const Base& base) = 0;
-    virtual Base* read(int id) = 0;
-    virtual bool update(const Base& base) = 0;
-    virtual bool deleter(int id) = 0;
+    virtual bool criar(const Base& base) = 0;
+    virtual Base* ler(int id) = 0;
+    virtual bool atualizar(const Base& base) = 0;
+    virtual bool excluir(int id) = 0;
 };
